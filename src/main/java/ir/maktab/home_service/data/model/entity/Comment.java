9ad7b406953cc2e1cbf018995dcfba7a8ad41a9a
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-01-29 15:25:49 +0330

    Comment.java class in home service

---
+++ src/main/java/ir/maktab/home_service/data/model/entity/Comment.java
@@ -0,0 +1,22 @@
+package ir.maktab.home_service.data.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment extends BaseEntity{
+
+    private Double score;
+
+    private String opinion;
+
+    @OneToOne
+    private Order order;
+}
