e6aa2d300d6d64279a9b6acc73af1935d430d150
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-01-29 15:25:20 +0330

    BaseServiceRepository.java class in home service

---
+++ src/main/java/ir/maktab/home_service/data/model/repository/BaseServiceRepository.java
@@ -0,0 +1,11 @@
+package ir.maktab.home_service.data.model.repository;
+
+import ir.maktab.home_service.data.model.entity.BaseService;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BaseServiceRepository extends CrudRepository<BaseService, Integer> {
+    Optional<BaseService> findByName(String name);}
