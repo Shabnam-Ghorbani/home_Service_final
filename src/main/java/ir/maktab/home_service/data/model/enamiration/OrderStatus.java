01bb8d0345c31f0e7fc86911414c6d07a38d35c3
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-01-29 15:31:49 +0330

    OrderStatus.java enum class in home service

---
+++ src/main/java/ir/maktab/home_service/data/model/enamiration/OrderStatus.java
@@ -0,0 +1,10 @@
+package ir.maktab.home_service.data.model.enamiration;
+
+public enum OrderStatus {
+    WAITING_FOR_EXPERT_SUGGESTIONS,
+    WAITING_FOR_EXPERT_SELECTION,
+    WAITING_FOR_THE_EXPERT_TO_ARRIVE,
+    STARTED,
+    DONE,
+    PAID;
+}
