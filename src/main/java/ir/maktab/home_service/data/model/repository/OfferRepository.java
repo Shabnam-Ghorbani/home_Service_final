d04177b20d9260b293fd905c62d9f8004072428a
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-01-29 15:29:23 +0330

    OfferRepository.java class in home service

---
+++ src/main/java/ir/maktab/home_service/data/model/repository/OfferRepository.java
@@ -0,0 +1,17 @@
+package ir.maktab.home_service.data.model.repository;
+
+import ir.maktab.home_service.data.model.entity.Expert;
+import ir.maktab.home_service.data.model.entity.Offer;
+import jakarta.persistence.criteria.Order;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OfferRepository extends PagingAndSortingRepository<Offer, Integer> {
+    List<Offer> findByOrder(Order order, Sort firstSort);
+
+    Optional<Offer> findByOrderAndExpert(Order order, Expert expert);}
