f4cbbf148bf6f0e89c01c0eb165b6852f1ca50fb
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-01-29 15:34:17 +0330

    UserRepository.java  class in home service

---
+++ src/main/java/ir/maktab/home_service/data/model/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ir.maktab.home_service.data.model.repository;
+
+import ir.maktab.home_service.data.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmailAddress(String email);}
