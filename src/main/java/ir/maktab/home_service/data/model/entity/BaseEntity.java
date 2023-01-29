3feb1f89b924cbc9a1dd3d0604686d345de8b74b
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-01-29 15:24:14 +0330

    BaseEntity class in home service

---
+++ src/main/java/ir/maktab/home_service/data/model/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package ir.maktab.home_service.data.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
