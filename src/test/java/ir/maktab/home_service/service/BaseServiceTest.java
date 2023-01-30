f6cee0d34553d0fd6ff9ac49067ec2c0e347d844
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-01-30 23:29:59 +0330

    BaseServiceTest in home Service system

---
+++ src/test/java/ir/maktab/home_service/service/BaseServiceTest.java
@@ -0,0 +1,48 @@
+package ir.maktab.home_service.service;
+
+import ir.maktab.home_service.data.model.entity.BaseService;
+import ir.maktab.home_service.exception.EntityIsExistException;
+import ir.maktab.home_service.exception.EntityNotExistException;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class BaseServiceTest {
+    @Autowired
+    private BaseServiceService baseServiceService;
+    BaseService baseService = BaseService.builder().name("Cleaning and hygiene").build();
+
+
+    @Test
+    @Order(1)
+    public void baseService_SaveTest() {
+        BaseService savedMainService = baseServiceService.save(baseService);
+        assertEquals(baseService, savedMainService);
+    }
+
+    @Test
+    @Order(2)
+    public void duplicateBaseService_Test() {
+        baseService = BaseService.builder().name("Cleaning and hygiene").build();
+        EntityIsExistException thrown = assertThrows(EntityIsExistException.class, () -> baseServiceService.save(baseService));
+        assertTrue(thrown.getMessage().contains("this baseService  exist!"));
+    }
+
+    @Test
+    @Order(3)
+    public void existBaseServiceName_Test() {
+        BaseService baseService1 = baseServiceService.findByName("Cleaning and hygiene");
+        assertNotNull(baseService1);
+    }
+
+    @Test
+    @Order(4)
+    public void NotExistBaseServiceName_Test() {
+        EntityNotExistException thrown = assertThrows(EntityNotExistException.class, () -> baseServiceService.findByName("mekanik"));
+        assertTrue(thrown.getMessage().contains("this baseService not exist!"));
+    }
+}
