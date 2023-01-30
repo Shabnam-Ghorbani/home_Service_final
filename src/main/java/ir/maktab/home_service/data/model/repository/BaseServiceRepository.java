d2ecc96e8b9e068cb07e37830c52b8dea0651220
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-01-30 23:28:29 +0330

    BaseServiceRepository in home Service system

--- src/main/java/ir/maktab/home_service/data/model/repository/BaseServiceRepository.java
+++ src/main/java/ir/maktab/home_service/data/model/repository/BaseServiceRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface BaseServiceRepository extends CrudRepository<BaseService, Integer> {
-    Optional<BaseService> findByName(String name);}
+    Optional<BaseService> findByName(String name);
+}
