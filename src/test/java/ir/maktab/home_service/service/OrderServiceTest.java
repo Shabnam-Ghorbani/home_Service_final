57c8758cbec7d9f2b1176e23ad3b9a7acbd3d78b
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-01 15:17:21 +0330

    OrderServiceTest in home Service system

---
+++ src/test/java/ir/maktab/home_service/service/OrderServiceTest.java
@@ -0,0 +1,60 @@
+package ir.maktab.home_service.service;
+
+import ir.maktab.home_service.data.model.enamiration.OrderStatus;
+import ir.maktab.home_service.data.model.entity.Address;
+import ir.maktab.home_service.data.model.entity.Customer;
+import ir.maktab.home_service.data.model.entity.Order;
+import ir.maktab.home_service.data.model.entity.SubService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OrderServiceTest {
+    @Autowired
+    CustomerService customerService;
+    @Autowired
+    SubServiceService subServiceService;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    AddressService addressService;
+    Order order;
+
+
+    @Test
+    @org.junit.jupiter.api.Order(1)
+    public void saveOrder_Test() {
+
+        Customer customer = customerService.findByEmailAddress("sabaabdi64@gmail.com");
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse("1400-10-15 12:30");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SubService subService = subServiceService.findById(3);
+        Address address = addressService.findByZipCode(89350382L);
+        order = Order.builder()
+                .proposedPrice(1000000L)
+                .jobDescription("description")
+                .dateOfWorkPerformed(date)
+                .address(address)
+                .customer(customer)
+                .orderStatus(OrderStatus.WAITING_FOR_EXPERT_SUGGESTIONS)
+                .subService(subService)
+                .build();
+
+        Order savedOrder = orderService.save(order);
+        assertEquals(order, savedOrder);
+    }
+}
