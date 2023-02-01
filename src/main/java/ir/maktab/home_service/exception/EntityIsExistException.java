07e390bfeb3a4133c8c9c3f8bc2ef0fe3070471f
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-01 15:11:20 +0330

    Exception.java in home Service system

--- src/main/java/ir/maktab/home_service/exception/EntityIsExistException.java
+++ src/main/java/ir/maktab/home_service/exception/EntityIsExistException.java
@@ -1,4 +1,5 @@
 package ir.maktab.home_service.exception;
+
 public class EntityIsExistException extends RuntimeException {
     public EntityIsExistException(String message) {
         super(message);
