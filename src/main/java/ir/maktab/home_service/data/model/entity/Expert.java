3d8496e93725081f920ca18b1d2b5eb2963acb77
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-01 15:11:50 +0330

    Expert.java in home Service system

--- src/main/java/ir/maktab/home_service/data/model/entity/Expert.java
+++ src/main/java/ir/maktab/home_service/data/model/entity/Expert.java
@@ -3,12 +3,11 @@ package ir.maktab.home_service.data.model.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Data
@@ -16,9 +15,10 @@ import java.util.Set;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Expert extends User implements Comparable<Expert> {
     @Lob
-    @Column(columnDefinition = "BLOB", length = 300000)
+    @Column(length = 300000)
     private byte[] photo;
     private Double score;
     @ManyToMany(mappedBy = "experts", fetch = FetchType.EAGER)
@@ -31,21 +31,21 @@ public class Expert extends User implements Comparable<Expert> {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Expert expert = (Expert) o;
-        return Arrays.equals(photo, expert.photo) && Objects.equals(score, expert.score) && Objects.equals(services, expert.services);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        if (!super.equals(o)) return false;
+//        Expert expert = (Expert) o;
+//        return Arrays.equals(photo, expert.photo) && Objects.equals(score, expert.score) && Objects.equals(services, expert.services);
+//    }
 
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(super.hashCode(), score, services);
-        result = 31 * result + Arrays.hashCode(photo);
-        return result;
-    }
+//    @Override
+//    public int hashCode() {
+//        int result = Objects.hash(super.hashCode(), score, services);
+//        result = 31 * result + Arrays.hashCode(photo);
+//        return result;
+//    }
 
     @Override
     public int compareTo(Expert o) {
