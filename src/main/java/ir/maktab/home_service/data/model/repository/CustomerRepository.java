f5983ca1aa521e180585ff7b9c0584ad7cdb6541
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:25:47 +0330

    CustomerRepository

--- src/main/java/ir/maktab/home_service/data/model/repository/CustomerRepository.java
+++ src/main/java/ir/maktab/home_service/data/model/repository/CustomerRepository.java
@@ -1,6 +1,8 @@
 package ir.maktab.home_service.data.model.repository;
 
 import ir.maktab.home_service.data.model.entity.Customer;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,16 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
     Optional<Customer> findByEmailAddress(String email);
+
+    @Modifying
+    @Query("update Customer c set c.credit = :newCredit where c.id = :id")
+    int updateCredit(Integer id, Long newCredit);
+
+    boolean existsByEmail(String emailAddress);
+
+    @Modifying
+    @Query(" update Customer c set c.password = :newPassword where c.id = :id")
+    int editPassword(Integer id, String newPassword);
+
+
 }
