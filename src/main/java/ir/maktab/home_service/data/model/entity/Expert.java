f35f79c7e85430d24d9bf1dd90a56a896e79adbc
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:30:15 +0330

    Expert

--- src/main/java/ir/maktab/home_service/data/model/entity/Expert.java
+++ src/main/java/ir/maktab/home_service/data/model/entity/Expert.java
@@ -1,16 +1,12 @@
 package ir.maktab.home_service.data.model.entity;
 
+import ir.maktab.home_service.data.model.enamiration.PersonStatus;
+import ir.maktab.home_service.data.model.enamiration.RoleStatus;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Data
 @Entity
@@ -18,18 +14,28 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class Expert extends User implements Comparable<Expert> {
+public class Expert extends Person implements Comparable<Expert> {
+    @Enumerated(value = EnumType.STRING)
+    PersonStatus personStatus;
+
+    @Enumerated(value = EnumType.STRING)
+    RoleStatus roleStatus;
     @Lob
     @Column(length = 300000)
     private byte[] photo;
     private Double score;
+    private Long credit;
     @ManyToMany(mappedBy = "experts", fetch = FetchType.EAGER)
     private Set<SubService> services = new HashSet<>();
 
     @Override
     public String toString() {
-        return super.toString() +
-                "score=" + score +
+        return "Expert{" +
+                "personStatus=" + personStatus +
+                ", photo=" + Arrays.toString(photo) +
+                ", score=" + score +
+                ", credit=" + credit +
+                ", services=" + services +
                 '}';
     }
 
@@ -39,14 +45,24 @@ public class Expert extends User implements Comparable<Expert> {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Expert expert = (Expert) o;
-        return Arrays.equals(photo, expert.photo) && Objects.equals(score, expert.score) && Objects.equals(services, expert.services);
+        return personStatus == expert.personStatus && Arrays.equals(photo, expert.photo) && Objects.equals(score, expert.score) && Objects.equals(credit, expert.credit) && Objects.equals(services, expert.services);
     }
 
     @Override
     public int hashCode() {
-        int result = Objects.hash(super.hashCode(), score, services);
+        int result = Objects.hash(super.hashCode(), personStatus, score, credit, services);
         result = 31 * result + Arrays.hashCode(photo);
         return result;
+    }
+
+    public void addSubService(SubService subService) {
+        this.services.add(subService);
+        subService.getExperts().add(this);
+    }
+
+    public void deleteSubService(SubService subService) {
+        this.services.remove(subService);
+        subService.getExperts().remove(this);
     }
 
     @Override
