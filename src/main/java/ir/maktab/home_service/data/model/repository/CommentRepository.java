12cc84b2afda5f5cd0e8b6666793b5fd9850841b
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:18:22 +0330

    comment repository

--- src/main/java/ir/maktab/home_service/data/model/repository/CommentRepository.java
+++ src/main/java/ir/maktab/home_service/data/model/repository/CommentRepository.java
@@ -1,9 +1,9 @@
 package ir.maktab.home_service.data.model.repository;
 
 import ir.maktab.home_service.data.model.entity.Comment;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends CrudRepository<Comment, Integer> {
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
 }
