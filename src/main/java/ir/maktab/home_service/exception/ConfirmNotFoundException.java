648a079090bd9a6c4de5ab2a43c4b4af1c99d4a0
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:20:09 +0330

    exception

---
+++ src/main/java/ir/maktab/home_service/exception/ConfirmNotFoundException.java
@@ -0,0 +1,7 @@
+package ir.maktab.home_service.exception;
+
+public class ConfirmNotFoundException extends RuntimeException {
+    public ConfirmNotFoundException(String message) {
+        super(message);
+    }
+}
