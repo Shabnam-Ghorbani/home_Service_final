07cc650c6b9148f71f6c3c5b150c4a213d7a7f8f
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:30:33 +0330

    Expert dto

---
+++ src/main/java/ir/maktab/home_service/dto/expertdto/ExpertActivationDTO.java
@@ -0,0 +1,12 @@
+package ir.maktab.home_service.dto.expertdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExpertActivationDTO {
+
+    Integer id;
+    Boolean isActive;
+}
