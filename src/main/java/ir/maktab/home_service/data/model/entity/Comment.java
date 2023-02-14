844965e0d4f12a74116ef7d9a7c5383f62fd9b74
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:17:36 +0330

    comment class

--- src/main/java/ir/maktab/home_service/data/model/entity/Comment.java
+++ src/main/java/ir/maktab/home_service/data/model/entity/Comment.java
@@ -1,6 +1,7 @@
 package ir.maktab.home_service.data.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,8 +17,19 @@ public class Comment extends BaseEntity {
 
     private Double score;
 
-    private String opinion;
+    private String comment;
 
     @OneToOne
+    @JoinColumn(name = "order_id")
+
     private Order order;
+
+    public Comment(Double score, String comment) {
+        this.score = score;
+        this.comment = comment;
+    }
 }
+
+
+
+
