e841011b5b4681567beabb2a3fd2dc64f3883d4d
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:14:41 +0330

    baseService dto for controller

---
+++ src/main/java/ir/maktab/home_service/dto/servicedto/BaseServiceDto.java
@@ -0,0 +1,14 @@
+package ir.maktab.home_service.dto.servicedto;
+
+import lombok.*;
+
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Setter
+@Getter
+public class BaseServiceDto {
+
+    private String name;
+}
