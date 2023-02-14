a9ae83a879fdad51d7fdb41888e030fcef567737
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:11:12 +0330

    implimation class address

--- src/main/java/ir/maktab/home_service/service/impl/AddressService.java
+++ src/main/java/ir/maktab/home_service/service/impl/AddressService.java
@@ -1,8 +1,9 @@
-package ir.maktab.home_service.service;
+package ir.maktab.home_service.service.impl;
 
 import ir.maktab.home_service.data.model.entity.Address;
 import ir.maktab.home_service.data.model.repository.AddressRepository;
 import ir.maktab.home_service.exception.EntityNotExistException;
+import ir.maktab.home_service.service.interf.AddInter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +11,16 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class AddressService {
+public class AddressService implements AddInter {
     private final AddressRepository addressRepository;
 
+    @Override
     public Address save(Address address) {
         Optional<Address> foundedAddress = addressRepository.findByZipCode(address.getZipCode());
         return foundedAddress.orElseGet(() -> addressRepository.save(address));
     }
 
+    @Override
     public Address findByZipCode(Long zipCode) {
         Optional<Address> address = addressRepository.findByZipCode(zipCode);
         return address.orElseThrow(() -> new EntityNotExistException("address not exist!"));
