a57ad1962911abd902f52c47b50507289933df5f
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-14 20:18:58 +0330

    interface comment

--- src/main/java/ir/maktab/home_service/service/impl/CommentService.java
+++ src/main/java/ir/maktab/home_service/service/impl/CommentService.java
@@ -1,16 +1,19 @@
-package ir.maktab.home_service.service;
+package ir.maktab.home_service.service.impl;
 
 import ir.maktab.home_service.data.model.entity.Comment;
 import ir.maktab.home_service.data.model.repository.CommentRepository;
+import ir.maktab.home_service.service.interf.CommInter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CommentService {
+public class CommentService implements CommInter {
     private final CommentRepository commentRepository;
 
+    @Override
     public void save(Comment comment) {
+
         commentRepository.save(comment);
     }
 }
