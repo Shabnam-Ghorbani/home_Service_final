a3e1bff34d0b51559321edacd1056b105df4e67f
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-16 17:15:09 +0330

    ExpertRepository

--- src/main/java/ir/maktab/home_service/data/model/repository/ExpertRepository.java
+++ src/main/java/ir/maktab/home_service/data/model/repository/ExpertRepository.java
@@ -1,6 +1,9 @@
 package ir.maktab.home_service.data.model.repository;
 
+import ir.maktab.home_service.data.model.enamiration.PersonStatus;
 import ir.maktab.home_service.data.model.entity.Expert;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,22 @@ import java.util.Optional;
 @Repository
 public interface ExpertRepository extends CrudRepository<Expert, Integer> {
     Optional<Expert> findByEmailAddress(String email);
+
+    Optional<Expert> findById(Integer id);
+
+    @Modifying
+    @Query("update Expert e set e.score = :newScore where e.id = :expertId")
+    Integer updateScore(Integer expertId, Integer newScore);
+
+    @Modifying
+    @Query("update Expert e set e.personStatus = :personStatus where e.emailAddress = :emailAddress")
+    int changePersonStatus(String emailAddress, PersonStatus personStatus);
+
+    @Modifying
+    @Query("update Expert e set e.isActive = :isActive where e.id = :id")
+    int changeExpertActivation(Integer id, Boolean isActive);
+
+    @Modifying
+    @Query("update Expert e set e.credit = :newCredit where e.id = :expertId ")
+    int updateCredit(Integer expertId, Long newCredit);
 }
