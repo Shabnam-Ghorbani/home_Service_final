b40f706b1c9eb0a8b147fb91e38aefe9d55fdaa3
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-16 21:01:22 +0330

    exception for Image

---
+++ src/main/java/ir/maktab/home_service/exception/ImageSizeException.java
@@ -0,0 +1,7 @@
+package ir.maktab.home_service.exception;
+
+public class ImageSizeException extends RuntimeException{
+    public ImageSizeException(String message) {
+        super(message);
+    }
+}
