1d3853fe8742ea65b0cb9fe54a9fadc56c002a0a
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-16 21:02:16 +0330

    exception

---
+++ src/main/java/ir/maktab/home_service/exception/InsufficientFoundsException.java
@@ -0,0 +1,7 @@
+package ir.maktab.home_service.exception;
+
+public class InsufficientFoundsException extends RuntimeException {
+    public InsufficientFoundsException(String message) {
+        super(message);
+    }
+}
