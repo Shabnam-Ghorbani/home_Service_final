ab0e7a27fac2a314610f7baf8f80baa5ea590b25
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-16 21:32:09 +0330

    SubService detail dto

---
+++ src/main/java/ir/maktab/home_service/dto/servicedto/SubServiceDetailDTO.java
@@ -0,0 +1,15 @@
+package ir.maktab.home_service.dto.servicedto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SubServiceDetailDTO {
+
+    private Integer id;
+
+    private Long basePrice;
+
+    private String description;
+}
