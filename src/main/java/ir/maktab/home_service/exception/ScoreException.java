99bf8646b02a1157479c8b4dcfb6e22bcf332c22
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-16 21:29:38 +0330

    ScoreException

---
+++ src/main/java/ir/maktab/home_service/exception/ScoreException.java
@@ -0,0 +1,9 @@
+package ir.maktab.home_service.exception;
+
+public class ScoreException extends RuntimeException {
+
+    public ScoreException(String message) {
+        super(message);
+    }
+}
+
