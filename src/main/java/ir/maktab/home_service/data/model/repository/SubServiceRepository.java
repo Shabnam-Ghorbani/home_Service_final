fd6218d2b335a43813f7c6a8f11012d10454c644
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-16 21:33:20 +0330

    SubService repository

--- src/main/java/ir/maktab/home_service/data/model/repository/SubServiceRepository.java
+++ src/main/java/ir/maktab/home_service/data/model/repository/SubServiceRepository.java
@@ -1,6 +1,8 @@
 package ir.maktab.home_service.data.model.repository;
 
 import ir.maktab.home_service.data.model.entity.SubService;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,11 @@ public interface SubServiceRepository extends CrudRepository<SubService, Integer
     Optional<SubService> findByName(String name);
 
     Optional<SubService> findById(Integer id);
+
+    int deleteSubServiceById(Integer id);
+
+
+    @Modifying
+    @Query("update SubService ss set ss.basePrice = :basePrice, ss.description = :description where ss.id = :id")
+    int editBasePriceAndDescription(Integer id, Long basePrice, String description);
 }
