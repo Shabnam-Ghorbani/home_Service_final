b342c098869e12cdb5ec281d3dc86bf68042df58
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-16 21:05:19 +0330

    offer class

--- src/main/java/ir/maktab/home_service/data/model/entity/Offer.java
+++ src/main/java/ir/maktab/home_service/data/model/entity/Offer.java
@@ -1,10 +1,12 @@
 package ir.maktab.home_service.data.model.entity;
 
 import ir.maktab.home_service.data.model.enamiration.OfferStatus;
+import ir.maktab.home_service.data.model.enamiration.TimeType;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -15,12 +17,15 @@ import java.util.Date;
 @ToString
 @EqualsAndHashCode
 public class Offer extends BaseEntity {
+    private String offer;
 
     @ManyToOne
     private Expert expert;
 
     @ManyToOne
     private Order order;
+
+    private Boolean isAccept;
 
     @CreationTimestamp
     private Date registrationDate;
@@ -32,7 +37,34 @@ public class Offer extends BaseEntity {
     @Temporal(TemporalType.TIMESTAMP)
     private Date startTime;
 
+    private LocalDateTime endTime;
+
+
     @Enumerated(EnumType.STRING)
     private OfferStatus offerStatus;
 
+    @Enumerated(value = EnumType.STRING)
+    private TimeType timeType;
+
+    private Integer durationTime;
+
+    public Offer(String offer, Long proposedPrice, TimeType timeType, Integer durationTime, LocalDateTime endTime) {
+        this.offer = offer;
+        this.proposedPrice = proposedPrice;
+        this.timeType = timeType;
+        this.durationTime = durationTime;
+        this.endTime = endTime;
+    }
+    @Override
+    public String toString() {
+        return "Offer {" +
+                "offer='" + offer + '\'' +
+                ", proposedPrice=" + proposedPrice +
+                ", timeType=" + timeType +
+                ", durationTime=" + durationTime +
+                ", isAccept=" + isAccept +
+                ", expertScore='" + expert.getScore() + '\'' +
+
+                "} ";
+    }
 }
