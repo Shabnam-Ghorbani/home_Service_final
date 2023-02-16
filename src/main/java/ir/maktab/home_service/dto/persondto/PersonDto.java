b7e3c38c0ec1508f5352ced2e79698181b31a1b9
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-16 21:23:21 +0330

    Person dto

---
+++ src/main/java/ir/maktab/home_service/dto/persondto/PersonDto.java
@@ -0,0 +1,25 @@
+package ir.maktab.home_service.dto.persondto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDto {
+    private String firstname;
+
+    private String lastname;
+
+    @Email(message = "the format of the email is incorrect!")
+    private String email;
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8}$",
+            message = "the password must contain numbers, lowercase and uppercase letters")
+    private String password;
+}
