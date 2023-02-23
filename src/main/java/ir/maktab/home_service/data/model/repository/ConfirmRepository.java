edcc0661ef719ccd5fb2067cdcfe2f5a9ee803c5
shabnam ghorbani <shabnamghorbani90@gmail.com>
2023-02-23 18:29:17 +0330

    ConfirmRepository

--- src/main/java/ir/maktab/home_service/data/model/repository/ConfirmRepository.java
+++ src/main/java/ir/maktab/home_service/data/model/repository/ConfirmRepository.java
@@ -1,13 +1,12 @@
 package ir.maktab.home_service.data.model.repository;
 
-import ir.maktab.home_service.token.ConfirmationToken;
+import ir.maktab.home_service.token.Confirmation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface ConfirmRepository extends JpaRepository<ConfirmationToken, Integer> {
+public interface ConfirmRepository extends JpaRepository<Confirmation, Integer> {
 
-    Optional<ConfirmationToken> findByConfirmationToken(String confirmationToken);
+    Optional<Confirmation> findByConfirmation(String confirmation);
 }
 
